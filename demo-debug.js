257dd71f239e9063da6a713e0c69ea8ccdd2257f
DESKTOP-V43MDD4\NGUYEN HIEU <hieunghuu08@gmail.com>
2022-11-03 16:50:11 +0700

    Build lesson 04 Git-HTML

---
+++ demo-debug.js
@@ -0,0 +1,11 @@
+let numberA = Number(prompt("Enter A:"))
+let numberB = Number(prompt("Enter B:"))
+
+let divAB = numberA / numberB;
+
+alert("Sum of a and b: " + divAB);
+
+let a = [1,2,3,4,5]
+for(var i = 0; i<=a.length+1; i++){
+    console.log(a[i]);
+}
